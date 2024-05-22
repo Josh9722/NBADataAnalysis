540d785b9b9b1a2ffdb7d01ee39d7d7f9eb124f4
Josh Wells <wellsy9722@gmail.com>
2024-05-22 20:27:30 +1000

    Ensure even 0.1 split between pos and neg cases

--- Modelling.py
+++ Modelling.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -7,17 +7,25 @@ from sklearn.metrics import accuracy_score
 from sklearn.cluster import KMeans
 
 class Modelling:
-    def __init__(self):
+    def __init__(self, feature_importances=None):
         self.models = {
             'knn': KNeighborsClassifier(),
             'decision_tree': DecisionTreeClassifier(),
-            'random_forest': RandomForestClassifier(n_estimators=100, random_state=42)
+            'random_forest': RandomForestClassifier(n_estimators=100)
         }
         self.best_params = {}
         self.trained_models = {}
 
+        if feature_importances is not None:
+            print("Feature importances provided.")
+            self.feature_importances = feature_importances
+        else:
+            print("Feature importances not provided. Setting to 1 for all features.")
+            self.feature_importances = 1
+
     def train_model(self, x_train, y_train, model_name, param_grid=None):
         model = self.models[model_name]
+
         if param_grid:
             grid_search = GridSearchCV(model, param_grid, cv=5, scoring='accuracy')
             grid_search.fit(x_train, y_train)
@@ -30,9 +38,25 @@ class Modelling:
         return model
 
     def evaluate_model(self, model, x_test, y_test):
+        # Make predictions
         predictions = model.predict(x_test)
+
+        # Calculate overall accuracy
         accuracy = accuracy_score(y_test, predictions)
-        return accuracy
+
+        # Calculate the number of correct predictions for each class
+        correct_1 = sum((predictions == 1) & (y_test == 1))
+        correct_0 = sum((predictions == 0) & (y_test == 0))
+
+        # Calculate the total number of predictions for each class
+        total_1 = sum(predictions == 1)
+        total_0 = sum(predictions == 0)
+
+        # Calculate the percentage of correct predictions for each class
+        percentage_correct_1 = (correct_1 / total_1) * 100 if total_1 > 0 else 0
+        percentage_correct_0 = (correct_0 / total_0) * 100 if total_0 > 0 else 0
+
+        return accuracy, percentage_correct_1, percentage_correct_0
 
     def perform_clustering(self, data, model_name, param_grid=None):
         if model_name == 'kmeans':
